570c22dc45149a86eaf38d91f29f8098ebfd5479
jalpeshrajani <jalpesh.rajani@gmail.com>
2025-11-07 12:28:31 +0530

    Base setup commit to run on macbook and android

--- app/src/main/java/com/example/mirroringapp/MainActivity.kt
+++ app/src/main/java/com/example/mirroringapp/MainActivity.kt
@@ -42,6 +42,7 @@ class MainActivity : ComponentActivity() {
         MirroringViewModel.Factory(application)
     }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
