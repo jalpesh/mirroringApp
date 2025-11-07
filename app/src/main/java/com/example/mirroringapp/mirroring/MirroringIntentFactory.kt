0af17bf6e188f3710e77740b95875b17e08e1a81
Jalpesh Rajani <jalpesh.rajani@gmail.com>
2025-11-07 12:43:51 +0530

    Remove unsupported projection source extras

--- app/src/main/java/com/example/mirroringapp/mirroring/MirroringIntentFactory.kt
+++ app/src/main/java/com/example/mirroringapp/mirroring/MirroringIntentFactory.kt
@@ -2,9 +2,7 @@ package com.example.mirroringapp.mirroring
 
 import android.app.Application
 import android.content.Intent
-import android.hardware.display.DisplayManager
 import android.media.projection.MediaProjectionManager
-import android.os.Build
 
 class MirroringIntentFactory(private val app: Application) {
 
@@ -12,10 +10,6 @@ class MirroringIntentFactory(private val app: Application) {
         app.getSystemService(MediaProjectionManager::class.java)
 
     fun createProjectionIntent(): Intent? {
-        return projectionManager?.createScreenCaptureIntent()?.apply {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-                putExtra(MediaProjectionManager.EXTRA_MEDIA_PROJECTION_SOURCE, MediaProjectionManager.PROJECTION_SOURCE_SCREEN)
-            }
-        }
+        return projectionManager?.createScreenCaptureIntent()
     }
 }
