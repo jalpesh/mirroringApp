37657020b3c7650d1337b0fe2e7e774dd658696b
jalpeshrajani <jalpesh.rajani@gmail.com>
2025-11-07 14:21:25 +0530

    Base setup commit to run on macbook and android

--- app/src/main/java/com/example/mirroringapp/mirroring/MirroringIntentFactory.kt
+++ app/src/main/java/com/example/mirroringapp/mirroring/MirroringIntentFactory.kt
@@ -1,15 +1,15 @@
 package com.example.mirroringapp.mirroring
 
-import android.app.Application
+import android.content.Context
 import android.content.Intent
 import android.hardware.display.DisplayManager
 import android.media.projection.MediaProjectionManager
 import android.os.Build
 
-class MirroringIntentFactory(private val app: Application) {
+class MirroringIntentFactory(private val context: Context) {
 
     private val projectionManager: MediaProjectionManager? =
-        app.getSystemService(MediaProjectionManager::class.java)
+        context.getSystemService(MediaProjectionManager::class.java)
 
     fun createProjectionIntent(): Intent? {
         return projectionManager?.createScreenCaptureIntent()
